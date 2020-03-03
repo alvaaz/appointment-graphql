ba2bb1d37c30ecbacc494684131979e99fee6e34
Álvaro <agoede@jumpitt.com>
2020-03-03 11:08:07 -0300

    add specialties query

--- src/graphql/resolvers/specialty.ts
+++ src/graphql/resolvers/specialty.ts
@@ -2,8 +2,23 @@ import { SpecialtyModel } from '../../specialty/specialty.model'
 import { Specialty } from '../../specialty/specialty.interface'
 import { Professional } from '../../professional/professional.interface'
 import { ProfessionalModel } from '../../professional/professional.model'
+import { professionals } from './merge'
 
 export default {
+  async Specialties(): Promise<Specialty[] | undefined> {
+    try {
+      const specialties = await SpecialtyModel.find()
+      return specialties.map(specialty => {
+        return {
+          _id: specialty._id,
+          name: specialty.name,
+          professionals: professionals.bind(this, specialty.professionals)
+        }
+      })
+    } catch (err) {
+      throw new Error(`Something goes wrong ${err}`)
+    }
+  },
   async createSpecialty({ specialtyInput }: { specialtyInput: Specialty }): Promise<Specialty> {
     try {
       const newSpecialty = new SpecialtyModel({
